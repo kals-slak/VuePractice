2302baa68201278c30495a882c02a9567446c8a0
Kalyana Sundaram <sundaram.kalyan369@gmail.com>
2023-03-13 21:07:24 +0530

    simple todo app

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+  </div>
+  
+  <div>
+    <form>
+      <input v-model="todo">
+      <button @click.prevent="addTodo">Add Todo</button>
+    </form>
+  </div>
+
+  <div class="list">
+    <div class="todo" v-for="t in todos" :key="{count}">
+      {{t}}
+    </div>
+  </div>
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+let todo= ref("");
+let todos= ref([]);
+let count= ref(0);
+ 
+const addTodo = ()=>{
+  todos.value.push(todo.value);
+  todo.value="";
+  count++;
+}
+</script>
